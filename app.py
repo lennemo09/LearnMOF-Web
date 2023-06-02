22d9019c19e8ba2e189b101817557e15a1a6d2e6
Len <lennemo09@gmail.com>
2023-06-02 14:21:22 +0700

    Now accepts zip file and multi-file uploads.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, send_from_directory
 import os
 import pandas as pd
 from pathlib import Path
@@ -7,6 +7,9 @@ from PIL import Image
 import numpy as np
 import torchvision.transforms as transforms
 import plotly.express as px
+import zipfile
+import pandas as pd
+import pymongo
 
 MODEL_NAME = "LearnMOF_MODEL_BATCH5_tested_acc95"
 
@@ -24,25 +27,75 @@ NUM_WORKERS = 1 # Number of CPU cores to load images
 
 app = Flask(__name__, static_url_path='/static')
 
+image_paths = []  # Global list to store the paths of uploaded images
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # Check if a file was submitted
-    if 'file' not in request.files:
-        return 'No file uploaded', 400
+    global image_paths
+    image_paths = []
+    
+     # Check if the 'images' field is present in the request files
+    if 'images' not in request.files:
+        return 'No images uploaded', 400
 
-    file = request.files['file']
+    images = request.files.getlist('images')  # Get a list of all uploaded image files
 
-    # Check if the file has a filename
-    if file.filename == '':
-        return 'No file selected', 400
+    # Check if any files were selected
+    if not images:
+        return 'No images selected', 400
 
-    # Save the file to the upload folder
-    file_path = app.config['UPLOAD_FOLDER'] + '/' + file.filename
-    file.save(file_path)
+    metadata_file = None
+     # Check if the 'metadata' field is present in the request files
+    if 'metadata' in request.files:
+        metadata_file = request.files['metadata']
+        print(metadata_file)
+        if metadata_file.filename.endswith('.csv'):
+            # Process the metadata CSV file
+            metadata_df = pd.read_csv(metadata_file)
+
+    for file in images:
+        # Check if the file has a filename
+        if file.filename == '':
+            return 'One or more files have no filename', 400
+
+        # Check if the file is a zip file
+        if file.filename.endswith('.zip'):
+            # Save the zip file to the upload folder
+            file_path = app.config['UPLOAD_FOLDER'] + '/' + file.filename
+            file.save(file_path)
+
+            # Extract the files from the zip archive
+            with zipfile.ZipFile(file_path, 'r') as zip_ref:
+                zip_ref.extractall(app.config['UPLOAD_FOLDER'])
+
+            # Get the list of extracted image files
+            extracted_files = [
+                str(app.config['UPLOAD_FOLDER']) + '/' + file
+                for file in zip_ref.namelist()
+                if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))
+            ]
+            image_paths.extend(extracted_files)
+        elif file.filename.endswith('.jpg'):
+            file_path = app.config['UPLOAD_FOLDER'] + '/' + file.filename
+            file.save(file_path)
+            image_paths.append(file_path)
+
+    # Redirect to the first result page
+    return redirect(url_for('result', index=0))
+
+
+@app.route('/result/<int:index>')
+def result(index):
+    index = int(request.args.get('index', 0))
+
+    if index < 0 or index >= len(image_paths):
+        return 'Invalid index', 400
+
+    file_path = image_paths[index]
 
     # Load and preprocess the image
     img = Image.open(file_path)
@@ -88,24 +141,27 @@ def upload():
     # {
     #   file_path : string;
     #   image_name : string;
-    #   upload_date : string;
     #   predicted_probabilities: [float,float,float]|null;
     #   assigned_label: string;
     #   label_approved: boolean;
     #   linker: string|null;
+    #   startdate : {year:int,month:int,day:int}|null;
+    #   plate_index : int|null;
+    #   image_index : int|null;
     #   magnification: int|null;
     #   conditions: {
     #       time: int|null;
     #       temp: float|null;
     #       ctot: float|null;
-    #       lmlogratio: float|null;
+    #       loglmratio: float|null;
     #   }
     # }
     result = {
         'image_path': file_path,
         'predicted_class': predicted_class,
         'probabilities': {class_names[i]: prediction[i] for i in range(len(prediction))},
-        'chart_html': chart_html
+        'chart_html': chart_html,
+        'index': index
     }
 
     new_db_entry = {
@@ -113,19 +169,34 @@ def upload():
         'image_path': file_path,
         'assigned-label': predicted_class,
         'approved' : False,
-        'probabilities': {class_names[i]: prediction[i] for i in range(len(prediction))},
+        'probabilities': {class_names[i]: float(prediction[i]) for i in range(len(prediction))},
     }
 
-    print(new_db_entry)
+    # print(new_db_entry)
+
+    # insert_result = collection.insert_one(new_db_entry)
 
     return render_template('result.html', result=result)
 
+@app.route('/<path:filename>')
+def uploaded_image(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 if __name__ == '__main__':
+    client = pymongo.MongoClient('mongodb://localhost:27017/')
+    db = client['learnmof']
+    collection = db['data']
+
+    print(db)
+
     # Setup device-agnostic code
     if torch.cuda.is_available():
         device = torch.device("cuda")
+        print(f"Using GPU {device} for PyTorch.")
     else:
         device = torch.device("cpu")
+        print("Using CPU for PyTorch.")
+    
 
     model = torch.load(MODEL_NAME,map_location=device)
     model = model.to(device)
@@ -148,6 +219,6 @@ if __name__ == '__main__':
     model.eval()
 
     # Set the upload folder
-    UPLOAD_FOLDER = MODEL_DIR + "/" + "static" + "/" + "images"
+    UPLOAD_FOLDER = 'static/images'
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-    app.run()
+    app.run(host="0.0.0.0")
